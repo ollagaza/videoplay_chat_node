c2b86d0b38d12a25779ec77fe8e8a7ef9b472278
hwj <hwj@mteg.co.kr>
2018-12-06 21:19:49 +0900

    일반 오류 처리 및 이메일 중복 체크 추가

--- src/models/member.model.js
+++ src/models/member.model.js
@@ -1,4 +1,5 @@
 import php from 'phpjs';
+import StdObject from '@/classes/StdObject';
 import ModelObject from '@/classes/ModelObject';
 
 export default class MemberModel extends ModelObject {
@@ -15,6 +16,11 @@ export default class MemberModel extends ModelObject {
 
   async create(params) {
     params = { ...params, password: php.md5(params.password) };
+
+    // 이메일이 중복되는 경우 409 CONFLICT를 뱉음
+    const { email_address } = params;
+    if (await this.findOne({ email_address }))
+      throw new StdObject(-1, '중복된 이메일 주소입니다.', 409);
 
     const result = await super.create(params);
 
