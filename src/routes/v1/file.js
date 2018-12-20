901c49fd469109056be0f4757421aad0adbd511f
hwj <hwj@mteg.co.kr>
2018-12-20 14:00:10 +0900

    file upload merge

--- src/routes/v1/file.js
+++ src/routes/v1/file.js
@@ -1,8 +1,26 @@
+import mkdirp from 'mkdirp';
+import path from 'path';
+import util from 'util';
+import multer from 'multer';
 import { Router } from 'express';
 import wrap from '@/utils/express-async';
 import StdObject from '@/classes/StdObject';
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve('/node-temp/temp/'))
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  },
+});
 const routes = Router();
+const upload = util.promisify(multer({
+  storage,
+  limits: {
+    fileSize: 1 * 1024 * 1024 * 1024, ///< 1GB 제한
+  }
+}).single('target'));
 
 /**
  * @brief 파일 리스팅
@@ -15,7 +33,11 @@ routes.get('/', wrap(async(req, res) => {
  * @brief 파일 업로드
  */
 routes.post('/', wrap(async(req, res) => {
+  console.log('up start');
+  await upload(req, res);
+  console.log(req.file);
 
+  res.json(new StdObject());
 }));
 
 /**
