1ef4da2a232337d575ea66cc2eea4b0836c00a78
hwang woo joong <weather8128@gmail.com>
2019-01-18 17:31:10 +0900

    연동 편의를 위한 get method 추가

--- src/routes/v1/trans.js
+++ src/routes/v1/trans.js
@@ -29,6 +29,9 @@ const routes = Router();
  *      smil_file_name:
  *        type: "string"
  *        description: "smil 파일 이름"
+ *      error:
+ *        type: "string"
+ *        description: "에러 정보"
  *    required:
  *      - success
  *
@@ -68,6 +71,72 @@ routes.put('/complete/:content_id', Auth.isAuthenticated(), Wrap(async(req, res)
   const trans_info = req.body;
   console.log(trans_info);
   const success = ("" + trans_info.success).toLowerCase();
+  const is_success = !trans_info ? false : (success === 'true' || success === '1');
+  if (is_success) {
+    if (Util.isEmpty(trans_info) || Util.isEmpty(trans_info.video_file_name) || Util.isEmpty(trans_info.smil_file_name)) {
+      throw new StdObject(1, '잘못된 파라미터', 400);
+    }
+
+    const operation_info = await new OperationModel({ database }).getOperationInfoByContentId(content_id);
+    if (!operation_info || operation_info.isEmpty()) {
+      throw new StdObject(2, '등록된 컨텐츠가 없습니다.', 400);
+    }
+    await new OperationMediaModel({ database }).updateTransComplete(operation_info, trans_info);
+  }
+
+  res.json(new StdObject());
+}));
+
+/**
+ * @swagger
+ * /trans/complete:
+ *  get:
+ *    summary: "트랜스코더의 진행 상태를 업데이트 한다."
+ *    tags: [Trans]
+ *    consumes:
+ *    - "application/json"
+ *    produces:
+ *    - "application/json"
+ *    parameters:
+ *    - name: "content_id"
+ *      in: "query"
+ *      description: "콘텐츠ID"
+ *      required: true
+ *      type: "string"
+ *    - name: success
+ *      in: "query"
+ *      type: "string"
+ *      description: "처리 결과. 성공: true or 1. 그 외 실패"
+ *      required: true
+ *    - name: video_file_name
+ *      in: "query"
+ *      type: "string"
+ *      description: "병합이 완료된 고화질 동영상 파일 이름"
+ *    - name: smil_file_name
+ *      in: "query"
+ *      type: "string"
+ *      description: "smil 파일 이름"
+ *    - name: error
+ *      in: "query"
+ *      type: "string"
+ *      description: "에러 정보"
+ *    responses:
+ *      200:
+ *        description: "성공여부"
+ *        schema:
+ *           $ref: "#/definitions/DefaultResponse"
+ *
+ */
+routes.get('/complete', Auth.isAuthenticated(), Wrap(async(req, res) => {
+
+  const content_id = req.query.content_id;
+  const trans_info = {
+    video_file_name: req.query.video_file_name,
+    smil_file_name: req.query.smil_file_name,
+    error: req.query.error,
+  };
+
+  const success = ("" + req.query.success).toLowerCase();
   const is_success = !trans_info ? false : (success === 'true' || success === '1');
   if (is_success) {
     if (Util.isEmpty(trans_info) || Util.isEmpty(trans_info.video_file_name) || Util.isEmpty(trans_info.smil_file_name)) {
