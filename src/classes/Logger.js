2abf0b04818228da8d3543c32da0dcf116400999
hwang woo joong <weather8128@gmail.com>
2019-01-21 20:52:12 +0900

    모든 로그 log4js로 통합. access log 추가.

--- src/classes/Logger.js
+++ src/classes/Logger.js
@@ -1,52 +1,56 @@
 import log4js from 'log4js';
 
+const IS_DEV = process.env.NODE_ENV === 'development';
 const logger_config = {
   appenders: {
     out: { type: 'stdout', layout: { type: 'coloured' } },
-    file: { type: 'dateFile', filename: '/logs/surgbook.log', pattern: '.yyyy-MM-dd', compress: false }
+    surgbook: { type: 'dateFile', filename: 'logs/surgbook.log', pattern: '.yyyy-MM-dd', compress: false },
+    access: { type: 'dateFile', filename: 'logs/access.log', pattern: '.yyyy-MM-dd', compress: false, "backups": 7 }
   },
   categories: {
-    default: { appenders: [ 'out', 'file' ], level: 'debug' }
+    default: { appenders: [ 'out', 'surgbook' ], level: IS_DEV ? 'DEBUG' : 'INFO' },
+    access: { appenders: [ 'out', 'access' ], level: 'ALL' }
   }
 };
 
-const logger = log4js.getLogger();
 log4js.configure(logger_config);
 
+const app_logger = log4js.getLogger('default');
 const getRequestUri = (request) => {
   return `[ ${request.method} ${request.originalUrl} ]\n`;
 };
 
 export default {
+  express: log4js.connectLogger(log4js.getLogger('access'), {"level": "INFO"}),
   "d": (request, ...args) => {
     if (request){
-      logger.debug(getRequestUri(request), ...args);
+      app_logger.debug(getRequestUri(request), ...args);
     } else {
-      logger.debug(...args);
+      app_logger.debug(...args);
     }
   },
 
   "w": (request, ...args) => {
     if (request){
-      logger.warn(getRequestUri(request), ...args);
+      app_logger.warn(getRequestUri(request), ...args);
     } else {
-      logger.warn(...args);
+      app_logger.warn(...args);
     }
   },
 
   "i": (request, ...args) => {
     if (request){
-      logger.info(getRequestUri(request), ...args);
+      app_logger.info(getRequestUri(request), ...args);
     } else {
-      logger.info(...args);
+      app_logger.info(...args);
     }
   },
 
   "e": (request, ...args) => {
     if (request){
-      logger.error(getRequestUri(request), ...args);
+      app_logger.error(getRequestUri(request), ...args);
     } else {
-      logger.error(...args);
+      app_logger.error(...args);
     }
   }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -74,15 +74,14 @@ const routes = Router();
  *
  */
 
-/**
- * in a one-shot manner. There should not be any reason to edit this file.
- */
 const files = require.context('.', true, /\/[^.]+\.js$/);
 
 files.keys().forEach((key) => {
   if (key === './index.js') return;
 
-  routes.use(key.replace(/(\.)|(js)/g, ''), files(key).default);
+  if (files(key).default) {
+    routes.use(key.replace(/(\.)|(js)/g, ''), files(key).default);
+  }
 });
 
 export default routes;
