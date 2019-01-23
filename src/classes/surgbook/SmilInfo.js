42dcafaa2ada871f67424f8db7d5041b882ae8e0
hwang woo joong <weather8128@gmail.com>
2019-01-23 18:13:34 +0900

    debug

--- src/classes/surgbook/SmilInfo.js
+++ src/classes/surgbook/SmilInfo.js
@@ -29,21 +29,28 @@ export default class SmilInfo extends JsonWrapper {
 
   loadFromXml = async (media_directory, smil_file_name) => {
     const smil_xml_info = await Util.loadXmlFile(media_directory + 'SEQ\\', smil_file_name);
+    console.log(smil_xml_info.body);
     if (smil_xml_info && smil_xml_info.body) {
-
       const body_node = _.isArray(smil_xml_info.body) ? smil_xml_info.body[0] : smil_xml_info.body;
       const switch_node = _.isArray(body_node.switch) ? body_node.switch[0] : body_node.switch;
       const video_node_list = switch_node.video;
 
+
+      console.log('step 1');
+
       if (video_node_list) {
+        console.log('step 2');
         for (let i = 0; i < video_node_list.length; i++) {
           const video_info = new SmilVideoInfo(video_node_list[i]);
           this.video_info_map[video_info.file_name] = video_info;
           this.video_info_list.push(video_info);
+          console.log(video_info);
         }
         this.is_empty = false;
       }
     }
+    console.log(this.video_info_map);
+    console.log(this.video_info_list);
 
     return this;
   };
