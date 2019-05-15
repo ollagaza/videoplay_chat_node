fe0eb5b88585e5c740d3e62b89e78cf9f92e7c98
hwang woo joong <weather8128@gmail.com>
2019-05-15 23:52:17 +0900

    자체 클러스터링 사용 암함. pm2를 통한 클러스터링 사용

--- src/index.js
+++ src/index.js
@@ -26,25 +26,25 @@ mongoose.connect('mongodb://mteg_vas:dpaxldlwl_!@localhost:27017/surgstory', { u
   .then(() => log.d(null, 'Successfully connected to mongodb'))
   .catch(e => log.e(null, e));
 
-if (IS_DEV) {
+// if (IS_DEV) {
   app.listen(PORT, () => log.d(null, `Listening on port ${PORT} -> PID: ${process.pid }`));
-} else {
-  //워커 스케쥴을 Round Robin 방식으로 한다.
-  cluster.schedulingPolicy = cluster.SCHED_RR;
-
-  if (cluster.isMaster) {
-    //CPU의 갯수만큼 워커 생성
-    os.cpus().forEach(function (cpu) {
-      cluster.fork();
-    });
-
-    // 자식이 죽었을 때 로그 기록
-    cluster.on('death', function (worker) {
-      log.e(null, 'worker ' + worker.pid + ' died'); // eslint-disable-line no-console
-    });
-  } else {
-    app.listen(PORT, () => log.d(null, `Listening on port ${PORT} -> PID: ${process.pid}`));
-  }
-}
+// } else {
+//   //워커 스케쥴을 Round Robin 방식으로 한다.
+//   cluster.schedulingPolicy = cluster.SCHED_RR;
+//
+//   if (cluster.isMaster) {
+//     //CPU의 갯수만큼 워커 생성
+//     os.cpus().forEach(function (cpu) {
+//       cluster.fork();
+//     });
+//
+//     // 자식이 죽었을 때 로그 기록
+//     cluster.on('death', function (worker) {
+//       log.e(null, 'worker ' + worker.pid + ' died'); // eslint-disable-line no-console
+//     });
+//   } else {
+//     app.listen(PORT, () => log.d(null, `Listening on port ${PORT} -> PID: ${process.pid}`));
+//   }
+// }
 
 service_config.load();
