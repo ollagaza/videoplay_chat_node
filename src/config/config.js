e693ad81a208ee4a02accb5d0188a935de82a8e8
hwang woo joong <weather8128@gmail.com>
2019-06-12 21:22:44 +0900

    4

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const ENV = process.env.NODE_ENV === 'development' ? 'development' : 'production';
 const IS_DEV = ENV === 'development';
-const LOG_PATH = process.env.LOG_PATH ? process.env.LOG_PATH : '../../logs';
+const LOG_PATH = process.env.LOG_PATH ? process.env.LOG_PATH : 'logs';
 
 export default {
   getEnv: () => {
