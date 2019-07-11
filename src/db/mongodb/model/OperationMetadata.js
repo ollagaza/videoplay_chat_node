91fe2a27ef6d651beba8ac012672ebe32fc8e7f0
hwang woo joong <weather8128@gmail.com>
2019-07-11 23:46:02 +0900

    수술 메타 데이터 저장 스키마 1차

--- src/db/mongodb/model/OperationMetadata.js
+++ src/db/mongodb/model/OperationMetadata.js
@@ -5,10 +5,16 @@ const Schema = mongoose.Schema;
 const getFieldInfos = () => {
   return {
     _id: { type: Number, index: true, require: false, message: '수술 아이디가 없습니다.' },
+    operation_seq: { type: Number, index: true, require: false, message: '사용자 아이디가 없습니다.' },
     member_seq: { type: Number, index: true, require: false, message: '사용자 아이디가 없습니다.' },
     content_id: { type: String, index: true, require: false, message: '콘텐츠 아이디가 없습니다.' },
     group_list: { type: [Number], default: [], require: false, message: '그룹 코드 목록이 없습니다.' },
     tag_list: { type: [String], default: [], require: false, message: '태그 목록이 없습니다.' },
+    machine_type_list: { type: [String], default: [], require: false, message: '사용 장비가 없습니다.' },
+    clinical_diagnosis_list: { type: [String], default: [], require: false, message: '임상 진단이 없습니다.' },
+    operation_diagnosis_list: { type: [String], default: [], require: false, message: '임상 증상이 없습니다.' },
+    clinical_symptoms_list: { type: [String], default: [], require: false, message: '수술중 진단이 없습니다.' },
+    final_diagnosis_list: { type: [String], default: [], require: false, message: '최종 진단이 없습니다.' },
     created_date: { type: Date, default: Date.now, require: false, message: '생성 일자가 없습니다.' },
     modify_date: { type: Date, default: Date.now, require: false, message: '수정 일자가 없습니다.' }
   };
@@ -19,22 +25,20 @@ schema_field_infos._id.require = true;
 schema_field_infos.member_seq.require = true;
 schema_field_infos.content_id.require = true;
 
-const OperationMetadataSchema = new Schema(schema_field_infos);
+const operation_metadata_schema = new Schema(schema_field_infos);
 
-OperationMetadataSchema.indexes();
-OperationMetadataSchema.index( { member_seq: 1, group_list: 1 } );
-OperationMetadataSchema.index( { member_seq: 1, tag_list: 1 } );
+operation_metadata_schema.indexes();
+operation_metadata_schema.index( { member_seq: 1, group_list: 1 } );
+operation_metadata_schema.index( { member_seq: 1, tag_list: 1 } );
+operation_metadata_schema.index( { member_seq: 1, clinical_symptoms_list: 1 } );
+operation_metadata_schema.index( { member_seq: 1, final_diagnosis_list: 1 } );
 
-OperationMetadataSchema.statics.createVideoProject = function( payload ) {
+operation_metadata_schema.statics.createVideoProject = function( payload ) {
   const model = new this(payload);
   return model.save();
 };
 
-OperationMetadataSchema.statics.updateFromEditor = function( id, payload ) {
-  return this.updateOne( { _id: id }, payload );
-};
-
-OperationMetadataSchema.statics.updateStatus = function( id, status ) {
+operation_metadata_schema.statics.updateStatus = function( id, status ) {
   const update = {
     status,
     modify_date: Date.now()
@@ -42,7 +46,7 @@ OperationMetadataSchema.statics.updateStatus = function( id, status ) {
   return this.updateOne( { _id: id }, update );
 };
 
-OperationMetadataSchema.statics.updateRequestStatus = function( id, request_status, progress = 0 ) {
+operation_metadata_schema.statics.updateRequestStatus = function( id, request_status, progress = 0 ) {
   const update = {
     request_status,
     progress,
@@ -51,7 +55,7 @@ OperationMetadataSchema.statics.updateRequestStatus = function( id, request_stat
   return this.findByIdAndUpdate( id, update );
 };
 
-OperationMetadataSchema.statics.updateRequestStatusByContentId = function( content_id, request_status, progress = 0, video_file_name = null, smil_file_name = null ) {
+operation_metadata_schema.statics.updateRequestStatusByContentId = function( content_id, request_status, progress = 0, video_file_name = null, smil_file_name = null ) {
   const update = {
     request_status,
     progress,
@@ -66,7 +70,7 @@ OperationMetadataSchema.statics.updateRequestStatusByContentId = function( conte
   return this.updateOne( { content_id: content_id }, update );
 };
 
-OperationMetadataSchema.statics.updateProgress = function( id, progress ) {
+operation_metadata_schema.statics.updateProgress = function( id, progress ) {
   const update = {
     progress,
     modify_date: Date.now()
@@ -74,25 +78,27 @@ OperationMetadataSchema.statics.updateProgress = function( id, progress ) {
   return this.updateOne( { _id: id }, update );
 };
 
-OperationMetadataSchema.statics.findOneById = function( id, projection = null ) {
+operation_metadata_schema.statics.findOneById = function( id, projection = null ) {
   return this.findById( id, projection );
 };
 
-OperationMetadataSchema.statics.findOneByContentId = function( content_id, projection = null ) {
+operation_metadata_schema.statics.findOneByContentId = function( content_id, projection = null ) {
   return this.findOne( { content_id: content_id }, projection );
 };
 
-OperationMetadataSchema.statics.findByMemberSeq = function( member_seq, projection = null ) {
+operation_metadata_schema.statics.findByMemberSeq = function( member_seq, projection = null ) {
   return this.find( { member_seq: member_seq }, projection );
 };
 
-OperationMetadataSchema.statics.findByOperationSeq = function( member_seq, operation_seq_list, projection = null ) {
+operation_metadata_schema.statics.findByOperationSeq = function( member_seq, operation_seq_list, projection = null ) {
   return this.find( { member_seq: member_seq, operation_seq_list: { "$in": operation_seq_list } }, projection );
 };
 
-OperationMetadataSchema.statics.deleteById = function( id ) {
+operation_metadata_schema.statics.deleteById = function( id ) {
   return this.findByIdAndDelete( id );
 };
 
-export const OperationMetadataModel = mongoose.model( 'OperationMetadata', OperationMetadataSchema );
+const operation_metadata_model = mongoose.model( 'OperationMetadata', operation_metadata_schema );
+
+export const OperationMetadataModel = operation_metadata_model;
 export const OperationMetadataField = getFieldInfos;
