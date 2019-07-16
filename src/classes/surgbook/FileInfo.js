025a1563585f512bbb9160c1269a47e616604791
hwang woo joong <weather8128@gmail.com>
2019-07-16 21:46:53 +0900

    동영상 업로드 파일 타입 체크 버그 수정.

--- src/classes/surgbook/FileInfo.js
+++ src/classes/surgbook/FileInfo.js
@@ -64,7 +64,7 @@ export default class FileInfo extends JsonWrapper {
     this.file_size = upload_file_info.size;
     this.file_path = media_path + Constants.SEP + upload_file_info.new_file_name;
 
-    const file_type = await Util.getFileType(this.file_path, this.file_name);
+    const file_type = await Util.getFileType(upload_file_info.path, this.file_name);
     this.file_type = file_type;
 
     this.is_empty = false;
