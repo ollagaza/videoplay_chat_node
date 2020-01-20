bfd095cbbebe3e85c9b64ce90f2559fdd254c9cf
hwang woo joong <weather8128@gmail.com>
2020-01-20 17:58:12 +0900

    서비스 singleton

--- src/service/service-config.js
+++ src/service/service-config.js
@@ -1,39 +1,45 @@
 import DBMySQL from '../database/knex-mysql'
 import ServiceConfigModel from '../database/mysql/service-config-model'
 
-let service_config = {};
+const ServiceConfigClass = class {
+  constructor () {
+    this.service_config_map = {}
+  }
 
-const load_config = async () => {
-  const service_config_model = new ServiceConfigModel( DBMySQL );
-  const config_list = await service_config_model.find();
+  load_config = async () => {
+    const service_config_model = new ServiceConfigModel( DBMySQL );
+    const config_list = await service_config_model.find();
 
-  service_config = {};
+    this.service_config_map = {};
 
-  if (config_list && config_list.length) {
-    for (let i = 0; i < config_list.length; i++) {
-      const config = config_list[i];
-      service_config[config.key] = config.value;
+    if (config_list && config_list.length) {
+      for (let i = 0; i < config_list.length; i++) {
+        const config = config_list[i];
+        this.service_config_map[config.key] = config.value;
+      }
     }
+    return true;
   }
-  return true;
-};
 
-export default {
-  init: async (callback) => {
-    await load_config();
+  init = async (callback) => {
+    await this.load_config();
     if (callback) callback();
-  },
-
-  reload: async (callback) => {
-    await load_config();
-    if (callback) callback();
-  },
-
-  getServiceInfo: () => {
-    return service_config;
-  },
-
-  get: (key) => {
-    return service_config[key];
   }
-};
+
+  reload = async (callback) => {
+    await this.load_config();
+    if (callback) callback();
+  }
+
+  getServiceInfo = () => {
+    return this.service_config_map;
+  }
+
+  get = (key) => {
+    return this.service_config_map[key];
+  }
+}
+
+const service_config = new ServiceConfigClass()
+
+export default service_config
