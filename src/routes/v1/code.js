053dd7dd1c4bc9299ecad734cce84df1202da42e
hwang woo joong <weather8128@gmail.com>
2020-02-11 17:13:48 +0900

    1

---
+++ src/routes/v1/code.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import Wrap from '../../utils/express-async';
+import Auth from '../../middlewares/auth.middleware';
+import Role from "../../constants/roles";
+import StdObject from '../../wrapper/std-object';
+import CodeSceneService from "../../service/code/CodeSceneService";
+
+const routes = Router();
+
+routes.get('/scene', Auth.isAuthenticated(Role.DEFAULT), Wrap(async (req, res, next) => {
+  const result = new StdObject();
+  result.add('result', CodeSceneService.getCodeMap());
+  res.json(result);
+}));
+
+export default routes;
