bdccd54e7df7eb0a68984805918eb943c71c1fba
Nan Jang Hyun <bobnam@mteg.co.kr>
2020-04-14 14:29:17 +0900

    로그인시 로그 남기도록 추가

--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -14,8 +14,8 @@ routes.post('/', Wrap(async(req, res) => {
   try {
     const member_info = await AuthService.login(DBMySQL, req)
     const output = await Auth.getTokenResult(res, member_info, Role.MEMBER);
-    // const ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
-    // await MemberLogService.createMemberLog(DBMySQL, member_info.seq, '0000', 'login', ip, 'N');
+    const ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
+    await MemberLogService.createMemberLog(DBMySQL, member_info.seq, '0000', 'login', ip, 'N');
     return res.json(output);
   } catch (e) {
     const output = e;
