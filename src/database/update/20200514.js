c7608324a643b8604196f3afeb5078286e55ffd7
Nan Jang Hyun <bobnam@mteg.co.kr>
2020-05-14 16:26:06 +0900

    테이블 스크립트 모음집 추가

---
+++ src/database/update/20200514.js
@@ -0,0 +1,14 @@
+const table_sendmail = `CREATE TABLE \`surgbook\`.\`sendmail\` (
+  \`seq\` TINYINT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  \`email_title\` VARCHAR(255) NULL,
+  \`email_desc\` TEXT NULL,
+  \`email_list\` JSON NULL,
+  \`member_seq\` INT(10) UNSIGNED NOT NULL,
+  \`regist_date\` TIMESTAMP NULL,
+  PRIMARY KEY (\`seq\`),
+  CONSTRAINT \`fk_sendmail_member_seq\`
+    FOREIGN KEY (\`member_seq\`)
+    REFERENCES \`surgbook\`.\`member\` (\`seq\`)
+    ON DELETE CASCADE
+    ON UPDATE RESTRICT);
+`
