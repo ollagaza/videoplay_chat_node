041fff5b196451c8bd324bda2d0bca906724bf0f
Nan Jang Hyun <bobnam@mteg.co.kr>
2020-06-11 11:21:19 +0900

    멘토링 및 마이페이지 작업중

---
+++ src/database/mysql/mypage/ProFileModel.js
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+import ServiceConfig from '../../../service/service-config';
+import Constants from '../../../constants/constants'
+import MySQLModel from '../../mysql-model'
+import Util from '../../../utils/baseutil'
+import StdObject from '../../../wrapper/std-object'
+import log from "../../../libs/logger";
+
+export default class ProFileModel extends MySQLModel {
+  constructor(database) {
+    super(database)
+
+    this.table_name = 'group_info'
+    this.selectable_fields = ['seq', 'profile', 'is_channel', 'is_mentoring']
+    this.log_prefix = '[ProFileModel]'
+  }
+
+  getProFileInfo = async group_seq => {
+    return this.findOne({ seq: group_seq }, this.selectable_fields);
+  };
+
+  updateProFileInfo = async (group_seq, profile) => {
+    return this.update({ seq: group_seq }, { profile });
+  }
+
+  updateChannelFlag = async (group_seq, param) => {
+    return this.update({ seq: group_seq }, { is_channel: param });
+  }
+
+  updateMentoFlag = async (group_seq, param) => {
+    return this.update({ seq: group_seq }, { is_mentoring: param });
+  }
+}
--- src/routes/v1/mypage.js
+++ src/routes/v1/mypage.js
@@ -5,6 +5,12 @@ import StdObject from '../../wrapper/std-object';
 import DBMySQL from '../../database/knex-mysql';
 import MemberLogModel from '../../database/mysql/member/MemberLogModel';
 import SocketManager from '../../service/socket-manager'
+import Role from "../../constants/roles";
+import Util from "../../utils/baseutil";
+import GroupService from '../../service/member/GroupService';
+import ProFileService from '../../service/mypage/ProFileService';
+import ServiceConfig from "../../service/service-config";
+import member_service from "../../service/member/MemberService";
 
 const routes = Router();
 
@@ -23,49 +29,61 @@ routes.post('/notice', Wrap(async(req, res) => {
   res.json(output);
 }));
 
-routes.post('/msg_list', Wrap(async(req, res) => {
-  req.accepts('application/json');
-  const user_seq = req.body.seq;
-  const output = new StdObject();
+routes.post('/managechanner', Auth.isAuthenticated(Role.LOGIN_USER), Wrap(async(req, res) => {
+  req.accepts('application/json')
+  const token_info = req.token_info
+  const group_seq = token_info.getGroupSeq()
+  const output = new StdObject()
+
+  output.add('group_counts', await GroupService.getGroupCountsInfo(DBMySQL, group_seq))
+  output.add('profile_info', await ProFileService.getProFileInfo(DBMySQL, group_seq))
 
   res.json(output);
 }));
 
-routes.get('/test_msg_send', Wrap(async(req, res) => {
-  req.accepts('application/json');
-  await SocketManager.sendToFrontOne('londhunter', { 'test': 'aaa' })
+routes.post('/updateprofile',
+  Auth.isAuthenticated(Role.LOGIN_USER),
+  Util.common_path_upload.fields([{ name: 'profile_image' }]), Wrap(async(req, res) => {
+    const token_info = req.token_info
+    const user_seq = token_info.getId();
+    const group_seq = token_info.getGroupSeq()
+    const profile = JSON.parse(req.body.profile);
+    const output = new StdObject()
 
-  const user_seq = req.body.seq;
-  const output = new StdObject();
+    try {
+      const member_info = await member_service.getMemberInfo(DBMySQL, user_seq);
+      const profile_dir = ServiceConfig.get('media_root') + '/' + member_info.user_id + '/profile';
+      const directory_exits = await Util.createDirectory(profile_dir);
+      const move_file = await Util.renameFile(req.files.profile_image[0].path, `${profile_dir}/${req.files.profile_image[0].filename}`)
 
-  res.json(output);
-}));
+      await DBMySQL.transaction(async (transaction) => {
+        profile.image = `/${member_info.user_id}/profile/${req.files.profile_image[0].filename}`;
+        const result = await ProFileService.updateProFileInfo(transaction, group_seq, JSON.stringify(profile));
+        output.add('result', result);
+        output.add('profile', profile);
+      });
+      res.json(output);
+    } catch (e) {
+      throw new StdObject(-1, e, 400);
+    }
+  }));
 
-routes.get('/test_glo_send', Wrap(async(req, res) => {
-  req.accepts('application/json');
-  const data = { 'type':'globalNotice', 'data':'test' };
-  await SocketManager.sendToFrontAll(data)
+routes.post('/changeGroupCMFlag', Auth.isAuthenticated(Role.LOGIN_USER), Wrap(async(req, res) => {
+  req.accepts('application/json')
+  const token_info = req.token_info
+  const group_seq = token_info.getGroupSeq()
+  const json_flag = req.body.data;
+  const output = new StdObject()
 
-  const user_seq = req.body.seq;
-  const output = new StdObject();
-
-  res.json(output);
-}));
-
-routes.post('/msg_send', Wrap(async(req, res) => {
-  req.accepts('application/json');
-  const user_seq = req.body.seq;
-  const output = new StdObject();
-
-  res.json(output);
-}));
-
-routes.post('/msg_receive', Wrap(async(req, res) => {
-  req.accepts('application/json');
-  const user_seq = req.body.seq;
-  const output = new StdObject();
-
-  res.json(output);
+  try {
+    await DBMySQL.transaction(async (transaction) => {
+      const result = await ProFileService.changeCMFlag(transaction, group_seq, json_flag);
+      output.add('result', result);
+    });
+    res.json(output);
+  } catch (e) {
+    throw new StdObject(-1, e, 400);
+  }
 }));
 
 export default routes;
