43283f606c3ffbbab1b0f1544759405902ebd039
Nan Jang Hyun <bobnam@mteg.co.kr>
2020-06-30 12:22:31 +0900

    profile_image_path 경로 변경

--- src/database/mysql/member/MemberModel.js
+++ src/database/mysql/member/MemberModel.js
@@ -45,7 +45,13 @@ export default class MemberModel extends MySQLModel {
   }
 
   getMemberInfo = async (member_seq) => {
-    const query_result = await this.findOne({seq: member_seq});
+    const oKnex = this.database.select('*')
+      .from(this.table_name)
+      .innerJoin('group_info', 'group_info.member_seq', 'member.seq')
+      .where('member.seq', member_seq)
+      .first();
+    // const query_result = await this.findOne({seq: member_seq});
+    const query_result = await oKnex;
     if (query_result && query_result.regist_date) {
       query_result.regist_date = Util.dateFormat(query_result.regist_date.getTime());
     }
