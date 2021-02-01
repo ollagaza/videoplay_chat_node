0bbc17328b889ef4252957e71ab04e0d5a1a1ff8
newXline <smileelf5@gmail.com>
2021-02-01 14:58:10 +0900

    그룹기본정보 가져오는 부분 select 항목 추가

--- src/database/mysql/group/GroupMemberModel.js
+++ src/database/mysql/group/GroupMemberModel.js
@@ -34,6 +34,7 @@ export default class GroupMemberModel extends MySQLModel {
       'group_info.seq AS group_seq', 'group_info.group_type', 'group_info.status AS group_status', 'group_info.group_name',
       'group_info.storage_size AS group_max_storage_size', 'group_info.used_storage_size AS group_used_storage_size', 'group_info.media_path',
       'group_info.profile_image_path', 'group_info.profile_image_path as profile_image_url', 'group_info.profile', 'group_info.is_set_group_name',
+      'group_info.search_keyword', 'group_info.group_explain', 'group_info.group_open', 'group_info.group_join_way', 'group_info.member_open', 'group_info.member_name_used',
       'group_info.reg_date', 'group_info.member_count'
     ]
 
