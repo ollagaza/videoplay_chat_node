0769d2d32b7654c95da8fc8d3139cc31c3026a8f
Nan Jang Hyun <bobnam@mteg.co.kr>
2021-03-09 18:57:41 +0900

    공용 업로드 경로 롤백

--- src/routes/v1/users.js
+++ src/routes/v1/users.js
@@ -66,9 +66,9 @@ routes.post('/', Util.common_path_upload.fields([{ name: 'profile_image' }, { na
 
   _.forEach(req.files, (value) => {
     if (value[0].fieldname === 'profile_image') {
-      params.user_info.profile_image_path = req.body.profile_image + '/' + value[0].filename
+      params.user_info.profile_image_path = '/common/' + value[0].filename
     } else if (value[0].fieldname === 'licens_image') {
-      params.user_sub_info.license_image_path = req.body.licens_image + '/' + value[0].filename
+      params.user_sub_info.license_image_path = '/common/' + value[0].filename
     }
   })
 
@@ -84,9 +84,9 @@ routes.post('/noCheckCreate', Util.common_path_upload.fields([{ name: 'profile_i
 
   _.forEach(req.files, (value) => {
     if (value[0].fieldname === 'profile_image') {
-      params.user_info.profile_image_path = req.body.profile_image + '/' + value[0].filename
+      params.user_info.profile_image_path = '/common/' + value[0].filename
     } else if (value[0].fieldname === 'licens_image') {
-      params.user_sub_info.license_image_path = req.body.licens_image + '/' + value[0].filename
+      params.user_sub_info.license_image_path = '/common/' + value[0].filename
     }
   })
 
@@ -111,9 +111,9 @@ routes.put('/:member_seq(\\d+)', Util.common_path_upload.fields([{ name: 'profil
 
   _.forEach(req.files, (value) => {
     if (value[0].fieldname === 'profile_image') {
-      params.user_info.profile_image_path = req.body.profile_image + '/' + value[0].filename
+      params.user_info.profile_image_path = '/common/' + value[0].filename
     } else if (value[0].fieldname === 'licens_image') {
-      params.user_sub_info.license_image_path = req.body.licens_image + '/' + value[0].filename
+      params.user_sub_info.license_image_path = '/common/' + value[0].filename
     }
   })
 
