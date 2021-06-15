8e547bf2460776df6eac0bfcffde2d7f1e79cf29
Nan Jang Hyun <bobnam@mteg.co.kr>
2021-06-15 15:32:25 +0900

    폴더 wrapper 필드 추가

--- src/wrapper/operation/OperationFolderInfo.js
+++ src/wrapper/operation/OperationFolderInfo.js
@@ -3,7 +3,7 @@ import JsonWrapper from '../json-wrapper'
 const default_key_list = [
   'seq', 'group_seq', 'member_seq', 'folder_name', 'folder_explain', 'parent_seq', 'parent_folder_list'
   , 'sort', 'status', 'depth', 'user_name', 'user_nickname'
-  , 'access_type', 'access_users', 'reg_date', 'modify_date', 'children', 'is_favorite', 'total_folder_size'
+  , 'access_type', 'is_access_way', 'access_users', 'reg_date', 'modify_date', 'children', 'is_favorite', 'total_folder_size'
   , 'is_delete_by_admin', 'delete_member_seq', 'delete_user_name', 'delete_user_nickname'
 ]
 
