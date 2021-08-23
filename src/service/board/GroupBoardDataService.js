0b6923d15dc2fd58cc9967dc1da4a63870917233
Nam Jang Hyun <bobnam@mteg.co.kr>
2021-08-23 16:10:47 +0900

    게시판 첨부파일 Object Storage 업로드 및 기존파일 다운로드 가능 하도록 수정

--- src/service/board/GroupBoardDataService.js
+++ src/service/board/GroupBoardDataService.js
@@ -9,6 +9,7 @@ import GroupBoardCommentModel from '../../database/mysql/board/GroupBoardComment
 import logger from "../../libs/logger";
 import GroupService from '../group/GroupService'
 import Constants from '../../constants/constants'
+import NaverObjectStorageService from "../storage/naver-object-storage-service";
 
 const GroupBoardDataServiceClass = class {
   constructor() {
@@ -307,6 +308,12 @@ const GroupBoardDataServiceClass = class {
       file_type: await Util.getFileType(upload_file_info.path, this.file_name),
       file_url: `${ServiceConfig.get('static_storage_prefix')}${upload_path}/${request.new_file_name}`
     }
+    if (!ServiceConfig.isVacs()) {
+      const storage_client = await NaverObjectStorageService.getStorageClient()
+      await NaverObjectStorageService.moveFile(`${upload_directory}${request.new_file_name}`, upload_path, request.new_file_name, ServiceConfig.get('naver_object_storage_bucket_name'), storage_client)
+      email_file_list.is_object_storage = true
+      email_file_list.download_cdn = `${ServiceConfig.get('cdn_url')}${upload_path}${request.new_file_name}`
+    }
     logger.debug(this.log_prefix, '[uploadFile]', `{ board_seq: ${board_seq} }`, 'email_file_list', email_file_list)
 
     let board_file_lists = JSON.parse(board_info.attach_file)
@@ -331,9 +338,14 @@ const GroupBoardDataServiceClass = class {
     const board_info = await this.getBoardDataDetail(database, board_seq)
     let board_file_lists = JSON.parse(board_info.attach_file)
     board_file_lists = _.reject(board_file_lists, file)
-    const media_root = ServiceConfig.get('media_root')
-    const file_full_path = `${media_root}${file.file_path}${file.file_name}`
-    await Util.deleteFile(file_full_path)
+    if (file.is_object_storage) {
+      const storage_client = await NaverObjectStorageService.getStorageClient()
+      await NaverObjectStorageService.deleteFile(`${file.file_path}`, file.file_name, ServiceConfig.get('naver_object_storage_bucket_name'), storage_client)
+    } else {
+      const media_root = ServiceConfig.get('media_root')
+      const file_full_path = `${media_root}${file.file_path}${file.file_name}`
+      await Util.deleteFile(file_full_path)
+    }
     const param = {
       attach_file: JSON.stringify(board_file_lists),
     }
