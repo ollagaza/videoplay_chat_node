7075471c42d57c8e653d37c6cb68277da92ce4b8
hwang woo joong <weather8128@gmail.com>
2021-11-25 15:48:10 +0900

    init 하기 전 mysql 접속 체크하는 로직 추가. 접속 실패 시 5초 후 다시 시도

--- src/service/init.js
+++ src/service/init.js
@@ -8,16 +8,35 @@ import SocketManager from './socket-manager'
 import VacsScheduler from '../scheduler/VacsScheduler'
 import MongoDataService from './common/MongoDataService'
 import SchedulerManager from './scheduler-manager'
-import Member from "./member_list";
+import Member from "./member_list"
+import kenx from '../database/knex-mysql'
+import DBMySQL from "../database/mysql-model"
+
 
 const initDirectories = async () => {
   await Util.createDirectory(ServiceConfig.get('common_root'))
   await Util.createDirectory(ServiceConfig.get('temp_directory_root'))
 }
 
+const checkConnectionDB = async () => {
+  const check_result = await kenx.select([kenx.raw("'Y' as `check`")]).first()
+  const is_connect_my_sql = check_result || check_result.check === 'Y'
+  if (is_connect_my_sql) {
+    log.debug('[InitService.checkConnectionDB]', 'MySQL Connect Success')
+  } else {
+    log.error('[InitService.checkConnectionDB]', 'MySQL Connect fail')
+  }
+  return check_result || check_result.check === 'Y'
+}
+
 export default {
   init: async () => {
     log.debug('[InitService]', 'init start')
+    let is_connect_my_sql = await checkConnectionDB()
+    while (!is_connect_my_sql) {
+      await Util.sleep(5000)
+      is_connect_my_sql = await checkConnectionDB()
+    }
     await MongoDB.init()
     await MongoDB.defaultMongoCollections()
     await ServiceConfig.init()
